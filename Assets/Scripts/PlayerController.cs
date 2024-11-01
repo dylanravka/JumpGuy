d4e4e9350a3530a7a7f1bcc7717ce3accbf9b9f9
dylan.ravka <dylanravka@hotmail.com>
2024-10-31 22:05:35 -0400

    added Jump Guy Part 1 Unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator playerAnim;
+    private Rigidbody2D rigidBody;
+
+    public float jumpForce;
+    public float movementSpeed;
+
+
+    private bool isJumping = false;
+
+    private Vector3 initScale = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+
+        initScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJumping)
+            {
+                rigidBody.AddForce(jumpForce * new Vector2(0.0f, 1.0f));
+                isJumping = true;
+                playerAnim.SetTrigger("Jump");
+                playerAnim.SetBool("isGrounded", false);
+            }
+        }
+
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        float horizontalMovement = horizontalInput * movementSpeed * Time.deltaTime;
+
+        if (horizontalInput != 0.0f)
+        {
+            float scaleX = horizontalInput < 0.0f ? -initScale.x : initScale.x;
+
+            transform.localScale = new Vector3(scaleX, initScale.y, initScale.z);
+
+
+        }
+
+        playerAnim.SetBool("isRunning", horizontalInput != 0.0f);
+
+
+        transform.Translate(new Vector3(horizontalMovement, 0.0f, 0.0f));
+
+        Debug.Log("HORIZONTAL MOVEMENT: " + horizontalMovement);
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+            playerAnim.SetBool("isGrounded", true);
+        }
+    }
+}
